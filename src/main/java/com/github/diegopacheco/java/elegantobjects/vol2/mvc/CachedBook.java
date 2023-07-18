9b1bb0772532a10a835aa0782fc76b28421b85bc
diegopacheco <diego.pacheco.it@gmail.com>
2023-07-17 23:28:15 -0700

    .

--- src/main/java/com/github/diegopacheco/java/elegantobjects/vol2/mvc/CachedBook.java
+++ src/main/java/com/github/diegopacheco/java/elegantobjects/vol2/mvc/CachedBook.java
@@ -7,16 +7,21 @@ public class CachedBook implements Manuscript {
 
     private Map<String,Book> cache = new ConcurrentHashMap<>();
     private String isbn;
+    private Manuscript manuscript;
 
-    public CachedBook(String isbn, Manuscript book){
+    public CachedBook(String isbn, Manuscript manuscript){
         cache.put("178646148X",new Book("Diego Pacheco",
                 "Building Applications with Scala", "178646148X"));
         this.isbn = isbn;
+        this.manuscript = manuscript;
     }
 
     @Override
     public String print() {
-        Book book = cache.get(isbn);
-        return book.print();
+        Book theBook = cache.get(isbn);
+        if (null==theBook){
+            return manuscript.print();
+        }
+        return theBook.print();
     }
 }
